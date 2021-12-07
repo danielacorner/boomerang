7855be94ef25722cd902abb853e20416aea4db5c
daniel.corner <danielcorner7@gmail.com>
2021-12-06 23:54:23 -0500

    tilt higher

--- src/components/Ground.tsx
+++ src/components/Ground.tsx
@@ -2,13 +2,23 @@ import { Plane } from "@react-three/drei";
 import { usePlane } from "@react-three/cannon";
 import { useBoomerangState } from "../store";
 
+const GROUND_PLANE = "groundPlane";
+
 export function Ground() {
   const [planeRef] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0] }));
 
   const [{ isThrown }, setState] = useBoomerangState();
   const onClick = (e) => {
-    const { x, y, z } = e.intersections[0].point;
-    const targetPosition = [x, y, z];
+    const ground = e.intersections.find((i) => i.object.name === GROUND_PLANE);
+    const point = ground?.point;
+    console.log("🌟🚨 ~ onClick ~ point", point);
+    if (!point) {
+      ("no intersection found!");
+      return;
+    }
+    console.log("🌟🚨 ~ onClick ~ point", point);
+    console.log("🌟🚨 ~ onClick ~ e.intersections", e.intersections);
+    const targetPosition = [point.x, 0.5, point.z];
     console.log(e);
     if (!isThrown) {
       setState((p) => ({
@@ -21,9 +31,10 @@ export function Ground() {
 
   return (
     <Plane
+      name={GROUND_PLANE}
       onClick={onClick}
       ref={planeRef}
-      args={[1000, 1000]}
+      args={[30, 30]}
       position={[0, -1, 0]}
       rotation={[-Math.PI / 2, 0, 0]}
       receiveShadow
