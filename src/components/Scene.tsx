2ecced391a04a75d880a32940aea96f03160f985
daniel.corner <danielcorner7@gmail.com>
2021-12-18 03:01:12 -0500

    sky

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -3,18 +3,21 @@ import { Ground } from "./Ground";
 import { Enemies } from "./Enemies/Enemies";
 import { Collisions } from "./Player/Collisions";
 import { Lighting } from "./Lighting";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Sky } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { atom, useAtom } from "jotai";
 import { useRef } from "react";
 
 export function Scene() {
+  const { x, y, z } = { x: -0.02, y: 0.01, z: 0 };
+  const { rayleigh } = { rayleigh: 0.7 };
   return (
     <mesh>
       <Ground />
       <Lighting />
       <Player />
       <Enemies />
+      <Sky sunPosition={[x, y, z]} rayleigh={rayleigh} />
       {/* <Collisions /> */}
       <OrbitControlsWithAngle />
     </mesh>
