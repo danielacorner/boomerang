1e52e19ffe4748a94fec918feeb4c0bafadf963d
daniel.corner <danielcorner7@gmail.com>
2021-12-19 01:25:56 -0500

    applyForce

--- src/components/Enemies/Enemy.tsx
+++ src/components/Enemies/Enemy.tsx
@@ -21,7 +21,6 @@ export function Enemy({ children, unmountEnemy }) {
   const [healthPercent, setHealthPercent] = useState(1);
   const theyDied = healthPercent === 0;
   const [theyreDead, setTheyreDead] = useState(false);
-  const [{ status }] = useBoomerangState();
 
   const [droppedMoneyPosition, setDroppedMoneyPosition] = useState<
     [number, number, number] | null
@@ -37,48 +36,12 @@ export function Enemy({ children, unmountEnemy }) {
     return unsubscribe;
   }, []);
 
-  const [enemyRef, api] = useCylinder(
-    () => ({
-      args: [3, 1, CYLINDER_HEIGHT, 6],
-      mass: 1,
-      position: position.current,
-      onCollide: (e) => {
-        if (status === "idle") return;
-        // when the boomerang+enemy collide, subtract some hp
-        const isCollisionWithBoomerang = e.body.name === BOOMERANG_NAME;
-        const isCollisionWithGround = e.body.name === GROUND_NAME;
-
-        setHealthPercent((prevHealthPct) => {
-          const _theyDied = prevHealthPct === 0;
-          // after they died, when they hit the ground again, they drop their moneys
-          const shouldDropMoneys = _theyDied && isCollisionWithGround;
-          if (shouldDropMoneys) {
-            setDroppedMoneyPosition((p) => p || position.current);
-          }
-
-          // subtract some hp when they hit the boomerang
-          if (isCollisionWithBoomerang) {
-            const nextHealthPercent = Math.max(
-              0,
-              prevHealthPct - BOOMERANG_DAMAGE
-            );
-            return nextHealthPercent;
-          }
-          return prevHealthPct;
-        });
-
-        console.log("COLLISION", e);
-      },
-      material: {
-        restitution: 1,
-        friction: 0,
-      },
-    }),
-    null,
-    [status]
-  );
-
-  useMoveEnemy(position, api, theyreDead);
+  const { enemyRef, api } = useMoveEnemy({
+    position,
+    theyreDead,
+    setHealthPercent,
+    setDroppedMoneyPosition,
+  });
 
   // TODO: they gotta drop their moneys
   // when they die, spin around, drop their moneys
@@ -113,7 +76,54 @@ export function Enemy({ children, unmountEnemy }) {
   );
 }
 
-function useMoveEnemy(position, api, theyreDead) {
+function useMoveEnemy({
+  position,
+  theyreDead,
+  setHealthPercent,
+  setDroppedMoneyPosition,
+}) {
+  const [{ status }] = useBoomerangState();
+
+  const [enemyRef, api] = useCylinder(
+    () => ({
+      args: [3, 1, CYLINDER_HEIGHT, 6],
+      mass: 1,
+      position: position.current,
+      onCollide: (e) => {
+        if (status === "idle") return;
+        // when the boomerang+enemy collide, subtract some hp
+        const isCollisionWithBoomerang = e.body.name === BOOMERANG_NAME;
+        const isCollisionWithGround = e.body.name === GROUND_NAME;
+
+        setHealthPercent((prevHealthPct) => {
+          const _theyDied = prevHealthPct === 0;
+          // after they died, when they hit the ground again, they drop their moneys
+          const shouldDropMoneys = _theyDied && isCollisionWithGround;
+          if (shouldDropMoneys) {
+            setDroppedMoneyPosition((p) => p || position.current);
+          }
+
+          // subtract some hp when they hit the boomerang
+          if (isCollisionWithBoomerang) {
+            const nextHealthPercent = Math.max(
+              0,
+              prevHealthPct - BOOMERANG_DAMAGE
+            );
+            return nextHealthPercent;
+          }
+          return prevHealthPct;
+        });
+
+        console.log("COLLISION", e);
+      },
+      material: {
+        restitution: theyreDead ? 1 : 0,
+        friction: theyreDead ? 0 : 100,
+      },
+    }),
+    null,
+    [status]
+  );
   useFrame(() => {
     if (!position.current || theyreDead) return;
     // random walk
@@ -128,17 +138,24 @@ function useMoveEnemy(position, api, theyreDead) {
       CYLINDER_HEIGHT / 2 + 1,
       position.current[2],
     ];
-    const [x2, y2, z2] = [
+
+    // TODO: use applyForce
+
+    const [fx, fy, fz] = [
       x + (Math.random() > 0.7 ? ENEMY_JITTER_SPEED * randomX * directionX : 0),
-      1,
+      0,
       z + (Math.random() > 0.4 ? ENEMY_JITTER_SPEED * randomZ * directionZ : 0),
     ];
 
-    const x2Lerp = THREE.MathUtils.lerp(x, x2, 0.1);
-    const y2Lerp = THREE.MathUtils.lerp(y, y2, 0.1);
-    const z2Lerp = THREE.MathUtils.lerp(z, z2, 0.1);
+    // const x2Lerp = THREE.MathUtils.lerp(x, x2, 0.1);
+    // const y2Lerp = THREE.MathUtils.lerp(y, y2, 0.1);
+    // const z2Lerp = THREE.MathUtils.lerp(z, z2, 0.1);
 
-    api.position.set(x2Lerp, y2Lerp, z2Lerp);
+    api.applyForce([fx - x, fy - y, fz - z], [x, y, z]);
+    // api.position.set(x2Lerp, y2Lerp, z2Lerp);
+    // api.position.set(x2Lerp, y2Lerp, z2Lerp);
     api.rotation.set(0, 0, 0);
   });
+
+  return { enemyRef, api };
 }
