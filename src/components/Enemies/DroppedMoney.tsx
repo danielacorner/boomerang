d007ffe8a430cc2d388bcaef66efe84fd277ee14
daniel.corner <danielcorner7@gmail.com>
2021-12-19 00:15:41 -0500

    moneybags animated

--- src/components/Enemies/DroppedMoney.tsx
+++ src/components/Enemies/DroppedMoney.tsx
@@ -2,6 +2,10 @@ import { useRef, useState } from "react";
 import MoneyBag from "../GLTFs/MoneyBag";
 import { useSpring, animated } from "@react-spring/three";
 import { useMount } from "react-use";
+import { useSphere } from "@react-three/cannon";
+
+const BAG_RADIUS = 0.4;
+
 export function DroppedMoney({ position }) {
   return (
     <group position={position}>
@@ -14,19 +18,43 @@ export function DroppedMoney({ position }) {
   );
 }
 function Bag() {
-  const [position, setPosition] = useState([0, 0, 0]);
-
+  const [mounted, setMounted] = useState(false);
+  const [ref, api] = useSphere(() => ({
+    mass: 0.1,
+    position: [0, 0, 0],
+    args: [BAG_RADIUS],
+  }));
   useMount(() => {
-    setPosition([
-      Math.random() * 5 - 2,
-      Math.random() * 5 - 2,
-      Math.random() * 5 - 2,
-    ]);
+    if (!ref.current) {
+      return;
+    }
+    setMounted(true);
+    const worldPoint: [number, number, number] = [
+      ref.current.position.x,
+      ref.current.position.y - BAG_RADIUS / 2,
+      ref.current.position.z,
+    ];
+    const kickUp: [number, number, number] = [
+      Math.random() * 0.5 - 0.25,
+      Math.random() * -2,
+      Math.random() * 0.5 - 0.25,
+    ];
+
+    api.applyImpulse(kickUp, worldPoint);
   });
 
-  const posAnimated = useSpring({ position });
+  const { opacity } = useSpring({
+    opacity: mounted ? 1 : 0,
+    config: { mass: 100, tension: 50, friction: 50 },
+  });
+
   return (
-    <animated.mesh scale={0.1} position={posAnimated as any}>
+    <animated.mesh
+      ref={ref}
+      scale={0.1}
+      material-transparent={true}
+      material-opacity={opacity}
+    >
       <MoneyBag />
     </animated.mesh>
   );
