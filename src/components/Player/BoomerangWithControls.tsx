ad2b2dd4edace1ae07536a261ed5ed34bc9acee9
dcorn068 <daniel@raventelemetry.com>
2021-12-20 20:46:20 -0500

    checkpoint

--- src/components/Player/BoomerangWithControls.tsx
+++ src/components/Player/BoomerangWithControls.tsx
@@ -12,135 +12,140 @@ const PLAYER_RADIUS = 3;
 const ROTATION_SPEED = -0.2;
 
 export const BoomerangWithControls = forwardRef(
-  (
-    { playerPosition }: { playerPosition: number[] },
-    playerRef: React.ForwardedRef<THREE.Mesh>
-  ) => {
-    const ref = useBoomerang(playerPosition, playerRef);
-    const [{ poweredUp }] = usePlayerState();
-    const { scale } = useSpring({ scale: poweredUp ? 4 : 1 });
-    return (
-      <animated.mesh
-        position={[0, -1, 0]}
-        ref={ref}
-        scale={scale}
-        name={BOOMERANG_NAME}
-      >
-        <Spin>
-          <BoomerangModel />
-        </Spin>
-        <FlashWhenStatusChanges />
-      </animated.mesh>
-    );
-  }
+	(
+		{ playerPosition }: { playerPosition: number[] },
+		playerRef: React.ForwardedRef<THREE.Mesh>
+	) => {
+		const ref = useBoomerang(playerPosition, playerRef);
+		const [{ poweredUp }] = usePlayerState();
+		const { scale } = useSpring({ scale: poweredUp ? 4 : 1 });
+		return (
+			<animated.mesh
+				position={[0, -1, 0]}
+				ref={ref}
+				scale={scale}
+				name={BOOMERANG_NAME}
+			>
+				<Spin>
+					<BoomerangModel />
+				</Spin>
+				<FlashWhenStatusChanges />
+			</animated.mesh>
+		);
+	}
 );
 
 function Spin({ children }) {
-  const ref = useRef<THREE.Mesh>(null);
+	const ref = useRef<THREE.Mesh>(null);
 
-  useFrame(() => {
-    if (!ref.current) return;
-    ref.current.rotation.set(0, ref.current.rotation.y + ROTATION_SPEED, 0);
-  });
+	useFrame(() => {
+		if (!ref.current) return;
+		ref.current.rotation.set(0, ref.current.rotation.y + ROTATION_SPEED, 0);
+	});
 
-  return <mesh ref={ref}>{children}</mesh>;
+	return <mesh ref={ref}>{children}</mesh>;
 }
 
 const INITIAL_POSITION: [number, number, number] = [0, 1, 0];
 
 /** shoots a boomerang when you click */
 function useBoomerang(playerPosition, playerRef) {
-  const [{ lookAt }] = usePlayerState();
-  const [boomerangRef, api] = useSphere(() => ({
-    mass: 1,
+	const [{ lookAt }] = usePlayerState();
+	const [boomerangRef, api] = useSphere(() => ({
+		mass: 1,
 
-    position: INITIAL_POSITION,
-    type: "Static", // https://github.com/pmndrs/use-cannon#types
-    // A static body does not move during simulation and behaves as if it has infinite mass. Static bodies can be moved manually by setting the position of the body. The velocity of a static body is always zero. Static bodies do not collide with other static or kinematic bodies.
-    material: {
-      restitution: 1,
-      friction: 0,
-    },
-  }));
+		position: INITIAL_POSITION,
+		type: "Static", // https://github.com/pmndrs/use-cannon#types
+		// A static body does not move during simulation and behaves as if it has infinite mass. Static bodies can be moved manually by setting the position of the body. The velocity of a static body is always zero. Static bodies do not collide with other static or kinematic bodies.
+		material: {
+			restitution: 1,
+			friction: 0,
+		},
+	}));
 
-  // boomerang state & click position
-  const [{ status, clickTargetPosition }, setBoomerangState] =
-    useBoomerangState();
+	// boomerang state & click position
+	const [{ status, clickTargetPosition }, setBoomerangState] =
+		useBoomerangState();
 
-  // subscribe to the position
-  const position = useRef(INITIAL_POSITION);
-  useEffect(() => {
-    const unsubscribe = api.position.subscribe((v) => (position.current = v));
-    return unsubscribe;
-  }, []);
+	// subscribe to the position
+	const position = useRef(INITIAL_POSITION);
+	useEffect(() => {
+		const unsubscribe = api.position.subscribe((v) => (position.current = v));
+		return unsubscribe;
+	}, []);
 
-  // move the boomerang
-  useFrame(() => {
-    if (!api || !playerPosition || !boomerangRef.current) {
-      console.log("🚨🚨 ~ no ref", playerPosition, api, boomerangRef);
-      return;
-    }
+	// move the boomerang
+	useFrame(() => {
+		if (!api || !playerPosition || !boomerangRef.current) {
+			console.log("🚨🚨 ~ no ref", playerPosition, api, boomerangRef);
+			return;
+		}
 
-    // target = the click position if it's outgoing, or the player if it's incoming
+		// target = the click position if it's outgoing, or the player if it's incoming
 
-    const target =
-      status === "flying"
-        ? clickTargetPosition
-        : playerPosition || [
-            playerRef.current?.position.x,
-            playerRef.current?.position.y,
-            playerRef.current?.position.z,
-          ];
+		const target =
+			status === "flying"
+				? clickTargetPosition
+				: playerPosition || [
+						playerRef.current?.position.x,
+						playerRef.current?.position.y,
+						playerRef.current?.position.z,
+				  ];
 
-    if (!target) {
-      console.log("🚨🚨 ~ no target!", target);
-      return;
-    }
+		if (!target) {
+			console.log("🚨🚨 ~ no target!", target);
+			return;
+		}
 
-    const [x, y, z] = position.current;
-    const [x2, y2, z2] = target;
-    const [dx, dy, dz] = [x2 - x, y2 - y, z2 - z];
-    let newX = 0;
-    let newY = 0;
-    let newZ = 0;
-    if (status === "flying") {
-      newX = THREE.MathUtils.lerp(x, x2, BOOMERANG_SPEED);
-      newY = THREE.MathUtils.lerp(y, y2, BOOMERANG_SPEED);
-      newZ = THREE.MathUtils.lerp(z, z2, BOOMERANG_SPEED);
-    } else {
-      newX = THREE.MathUtils.lerp(
-        // on return, we invert the speed: speed up over time instead of slowing down
-        x,
-        x2 - Math.abs(dx) * 0.1,
-        BOOMERANG_SPEED
-      );
-      newY = THREE.MathUtils.lerp(y, y2 - dy * 0.1, BOOMERANG_SPEED);
-      newZ = THREE.MathUtils.lerp(z, z2 - dz * 0.1, BOOMERANG_SPEED);
-    }
+		const [x, y, z] = position.current;
+		const [x2, y2, z2] = target;
 
-    const isAtTarget =
-      clickTargetPosition &&
-      Math.abs(newX - clickTargetPosition[0]) < 0.1 &&
-      Math.abs(newY - clickTargetPosition[1]) < 0.1 &&
-      Math.abs(newZ - clickTargetPosition[2]) < 0.1;
+		// TODO: apply impulse at first, then pull towards player
+		// TODO: initial velocity + pull towards player
 
-    const isAtPlayer =
-      playerPosition &&
-      Math.abs(newX - playerPosition[0]) < PLAYER_RADIUS &&
-      Math.abs(newY - playerPosition[1]) < PLAYER_RADIUS &&
-      Math.abs(newZ - playerPosition[2]) < PLAYER_RADIUS;
+		const [dx, dy, dz] = [x2 - x, y2 - y, z2 - z];
+		let newX = 0;
+		let newY = 0;
+		let newZ = 0;
+		if (status === "flying") {
+			newX = THREE.MathUtils.lerp(x, x2, BOOMERANG_SPEED);
+			newY = THREE.MathUtils.lerp(y, y2, BOOMERANG_SPEED);
+			newZ = THREE.MathUtils.lerp(z, z2, BOOMERANG_SPEED);
+		} else {
+			newX = THREE.MathUtils.lerp(
+				// on return, we invert the speed: speed up over time instead of slowing down
+				x,
+				x2 - Math.abs(dx) * 0.1,
+				BOOMERANG_SPEED
+			);
+			newY = THREE.MathUtils.lerp(y, y2 - dy * 0.1, BOOMERANG_SPEED);
+			newZ = THREE.MathUtils.lerp(z, z2 - dz * 0.1, BOOMERANG_SPEED);
+		}
 
-    if (isAtTarget && status === "flying") {
-      setBoomerangState((p) => ({ ...p, status: "returning" }));
-    } else if (isAtPlayer && status === "returning") {
-      setBoomerangState((p) => ({
-        ...p,
-        status: "idle",
-      }));
-    } else {
-      api.position.set(newX, newY, newZ);
-    }
-  });
+		const isAtTarget =
+			clickTargetPosition &&
+			Math.abs(newX - clickTargetPosition[0]) < 0.1 &&
+			Math.abs(newY - clickTargetPosition[1]) < 0.1 &&
+			Math.abs(newZ - clickTargetPosition[2]) < 0.1;
 
-  return boomerangRef;
+		const isAtPlayer =
+			playerPosition &&
+			Math.abs(newX - playerPosition[0]) < PLAYER_RADIUS &&
+			Math.abs(newY - playerPosition[1]) < PLAYER_RADIUS &&
+			Math.abs(newZ - playerPosition[2]) < PLAYER_RADIUS;
+
+		if (isAtTarget && status === "flying") {
+			setBoomerangState((p) => ({ ...p, status: "returning" }));
+		} else if (isAtPlayer && status === "returning") {
+			setBoomerangState((p) => ({
+				...p,
+				status: "idle",
+			}));
+		} else {
+			api.applyForce([x, y, z], [newX, newY, newZ]);
+			// api.position.set(newX, newY, newZ);
+		}
+	});
+
+	return boomerangRef;
 }
