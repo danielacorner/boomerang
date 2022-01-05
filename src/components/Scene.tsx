e1987d0367412c1cf8307dcb04bc3972cc3fef30
daniel.corner <danielcorner7@gmail.com>
2022-01-05 01:29:23 -0500

    start levels

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,21 +1,22 @@
 import { Player } from "./Player/Player";
 import { Ground } from "./Ground";
 import { Enemies } from "./Enemies/Enemies";
-import { Collisions } from "./Player/Collisions";
+import { Walls } from "./Player/Walls";
 import { Lighting } from "./Lighting";
 import { OrbitControlsWithAngle } from "./OrbitControlsWithAngle";
 import { DroppedItems } from "./DroppedItems/DroppedItems";
 
 export function Scene() {
-	return (
-		<mesh>
-			<Lighting />
-			<Player />
-			<Enemies />
-			<Collisions />
-			<OrbitControlsWithAngle />
-			<DroppedItems />
-		</mesh>
-	);
+  return (
+    <mesh>
+      <Lighting />
+      <Player />
+      <Enemies />
+      {/* <Walls /> */}
+      <Levels />
+      <OrbitControlsWithAngle />
+      <DroppedItems />
+    </mesh>
+  );
 }
 export type Direction = "ArrowUp" | "ArrowLeft" | "ArrowRight" | "ArrowDown";
