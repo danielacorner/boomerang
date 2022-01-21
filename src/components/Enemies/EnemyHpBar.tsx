87d37cad7379236075c4571e712821661f725639
daniel.corner <danielcorner7@gmail.com>
2022-01-21 00:26:47 -0500

    workin on hp bar

--- src/components/Enemies/EnemyHpBar.tsx
+++ src/components/Enemies/EnemyHpBar.tsx
@@ -1,6 +1,5 @@
-import { Billboard } from "@react-three/drei";
+import { Billboard, Text } from "@react-three/drei";
 import { useControls } from "leva";
-import { Nametag } from "./Nametag";
 
 export function EnemyHpBar({
   health,
@@ -11,11 +10,12 @@ export function EnemyHpBar({
 }) {
   const { x, y, z } = useControls({ x: 0, y: 8.4, z: 0.5 });
   return (
-    <Billboard renderOrder={3}>
+    <Billboard renderOrder={1}>
       {/* backdrop */}
       <mesh scale={1} position={[0, enemyHeight, 0]}>
         <boxGeometry attach="geometry" args={[maxHp, 1.4, 0.1]} />
         <meshBasicMaterial
+          depthTest={false}
           attach="material"
           color={"#350101"}
           transparent={true}
@@ -27,6 +27,7 @@ export function EnemyHpBar({
       <mesh scale={1} position={[0, enemyHeight + 0.01, 0.01]}>
         <boxGeometry attach="geometry" args={[health, 1.4, 0.1]} />
         <meshBasicMaterial
+          depthTest={false}
           attach="material"
           color={"#810f0f"}
           transparent={true}
@@ -35,5 +36,45 @@ export function EnemyHpBar({
         <Nametag name={enemyName} url={enemyUrl} />
       </mesh>
     </Billboard>
+  );
+}
+
+function Nametag({ name, url }) {
+  return (
+    <mesh renderOrder={1} material-depthTest={false} position={[0, 0, 0]}>
+      <Text
+        anchorY={"middle"}
+        color={"#ffffff"}
+        fontSize={1}
+        scale={1}
+        position={[0, -0.05, 0.3]}
+      >
+        {name}
+      </Text>
+      {/* <Html
+          style={{
+            fontSize: "1.2rem",
+            width: "fit-content",
+            textAlign: "center",
+            whiteSpace: "nowrap",
+            display: "flex",
+            justifyContent: "center",
+            transform: "translateX(-50%)",
+          }}
+        >
+          <a
+            style={{
+              color: "white",
+              cursor: "pointer",
+              textDecoration: "none",
+            }}
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {name}
+          </a>
+        </Html> */}
+    </mesh>
   );
 }
