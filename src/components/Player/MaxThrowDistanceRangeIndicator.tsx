4926605365d3109e034f8a6732ecf1d54b93191c
daniel.corner <danielcorner7@gmail.com>
2022-02-05 01:49:00 -0500

    separate MaxThrowDistanceRangeIndicator from mage

--- src/components/Player/MaxThrowDistanceRangeIndicator.tsx
+++ src/components/Player/MaxThrowDistanceRangeIndicator.tsx
@@ -1,42 +1,58 @@
 import { BoomerangTarget } from "./Boomerang/BoomerangTarget";
-import { useHeldBoomerangs, usePlayerState } from "../../store";
+import {
+  useHeldBoomerangs,
+  usePlayerPositionRef,
+  usePlayerState,
+} from "../../store";
 import { MAX_THROW_DISTANCE } from "../../utils/constants";
 import { BoomerangWithControls } from "./Boomerang/BoomerangWithControls";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
 /** a white torus of radius MAX_THROW_DISTANCE */
 export function MaxThrowDistanceRangeIndicator() {
-	const [{ rangeUp }] = usePlayerState();
-	// if it's above the max distance, shrink it down to the max distance
-	const maxThrowDistance = MAX_THROW_DISTANCE * (rangeUp ? 3 : 1);
-	return (
-		<mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-			<torusBufferGeometry
-				attach="geometry"
-				args={[maxThrowDistance, 0.2, 8, 32]}
-			/>
-			<meshBasicMaterial
-				attach="material"
-				transparent={true}
-				opacity={0.15}
-				color={0xdcfcfc}
-			/>
-		</mesh>
-	);
+  const [{ rangeUp }] = usePlayerState();
+  // if it's above the max distance, shrink it down to the max distance
+  const maxThrowDistance = MAX_THROW_DISTANCE * (rangeUp ? 3 : 1);
+  const [playerPositionRef] = usePlayerPositionRef();
+  const ref = useRef<THREE.Mesh | null>(null);
+  useFrame(() => {
+    if (!ref.current) return;
+    ref.current.position.set(
+      playerPositionRef.current[0],
+      playerPositionRef.current[1],
+      playerPositionRef.current[2]
+    );
+  });
+  return (
+    <mesh ref={ref} rotation={[-Math.PI / 2, 0, 0]}>
+      <torusBufferGeometry
+        attach="geometry"
+        args={[maxThrowDistance, 0.2, 8, 32]}
+      />
+      <meshBasicMaterial
+        attach="material"
+        transparent={true}
+        opacity={0.15}
+        color={0xdcfcfc}
+      />
+    </mesh>
+  );
 }
 export function Boomerang() {
-	const [heldBoomerangs] = useHeldBoomerangs();
+  const [heldBoomerangs] = useHeldBoomerangs();
 
-	return (
-		<>
-			{heldBoomerangs.map((_, idx) => (
-				<BoomerangWithControls
-					key={idx}
-					{...{
-						idx,
-					}}
-				/>
-			))}
-			<BoomerangTarget />
-		</>
-	);
+  return (
+    <>
+      {heldBoomerangs.map((_, idx) => (
+        <BoomerangWithControls
+          key={idx}
+          {...{
+            idx,
+          }}
+        />
+      ))}
+      <BoomerangTarget />
+    </>
+  );
 }
--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -2,16 +2,16 @@ import BlackMage from "../GLTFs/BlackMage";
 import { MouseTarget } from "./MouseTarget";
 import { useEffect, useState } from "react";
 import {
-	useGameState,
-	usePlayerPositionRef,
-	usePlayerRef,
-	usePlayerState,
+  useGameState,
+  usePlayerPositionRef,
+  usePlayerRef,
+  usePlayerState,
 } from "../../store";
 import { useSpring, animated } from "@react-spring/three";
 import { PLAYER_NAME } from "../../utils/constants";
 import {
-	Boomerang,
-	MaxThrowDistanceRangeIndicator,
+  Boomerang,
+  MaxThrowDistanceRangeIndicator,
 } from "./MaxThrowDistanceRangeIndicator";
 import { usePlayerControls } from "./usePlayerControls";
 import { RangeupCircularTimer } from "./RangeupCircularTimer";
@@ -21,82 +21,82 @@ import { Html } from "@react-three/drei";
 import { useInterval } from "react-use";
 
 export function Player() {
-	return (
-		<>
-			<Mage />
-			<Boomerang />
-			<RangeupCircularTimer />
-			<PowerupCircularTimer />
-			<RangeupIndicator />
-			<Target />
-			<Controls />
-		</>
-	);
+  return (
+    <>
+      <Mage />
+      <Boomerang />
+      <RangeupCircularTimer />
+      <PowerupCircularTimer />
+      <RangeupIndicator />
+      <Target />
+      <Controls />
+      <MaxThrowDistanceRangeIndicator />
+    </>
+  );
 }
 
 function Controls() {
-	usePlayerControls();
+  usePlayerControls();
 
-	return null;
+  return null;
 }
 const Target = () => {
-	return <MouseTarget />;
+  return <MouseTarget />;
 };
 
 function Mage() {
-	const { scale, opacity } = useMageSpring();
+  const { scale, opacity } = useMageSpring();
 
-	const [playerRef] = usePlayerRef();
+  const [playerRef] = usePlayerRef();
 
-	return (
-		<animated.mesh
-			scale={scale}
-			material-transparent={true}
-			material-opacity={opacity}
-			ref={playerRef}
-			name={PLAYER_NAME}
-		>
-			{process.env.NODE_ENV === "development" && <PositionIndicator />}
-			<BlackMage position={[0, -1, 0]} rotation={[0, Math.PI, 0]} />
-			<MaxThrowDistanceRangeIndicator />
-			<pointLight intensity={5} distance={24} />
-		</animated.mesh>
-	);
+  return (
+    <animated.mesh
+      scale={scale}
+      material-transparent={true}
+      material-opacity={opacity}
+      ref={playerRef}
+      name={PLAYER_NAME}
+    >
+      {process.env.NODE_ENV === "development" && <PositionIndicator />}
+      <BlackMage position={[0, -1, 0]} rotation={[0, Math.PI, 0]} />
+      <pointLight intensity={5} distance={24} />
+    </animated.mesh>
+  );
 }
 function PositionIndicator() {
-	const [playerPositionRef] = usePlayerPositionRef();
-	const [key, setkey] = useState(0);
-	useInterval(() => {
-		setkey(Math.random());
-	}, 100);
-	return (
-		<Html position={[0, -2, 0]} style={{ color: "white" }} key={key}>
-			{playerPositionRef.current.map((p) => p.toFixed(1)).join(",")}
-		</Html>
-	);
+  const [playerPositionRef] = usePlayerPositionRef();
+  const [key, setkey] = useState(0);
+  useInterval(() => {
+    setkey(Math.random());
+  }, 100);
+  return (
+    <Html position={[0, -2, 0]} style={{ color: "white" }} key={key}>
+      {playerPositionRef.current.map((p) => p.toFixed(1)).join(",")}
+    </Html>
+  );
 }
 function useMageSpring() {
-	const [{ poweredUp }] = usePlayerState();
-	const [{ invulnerable }] = useGameState();
+  const [{ poweredUp }] = usePlayerState();
+  const [{ invulnerable }] = useGameState();
 
-	const [blinkOn, setBlinkOn] = useState(false);
+  const [blinkOn, setBlinkOn] = useState(false);
 
-	useEffect(() => {
-		if (invulnerable) {
-			setBlinkOn(true);
-		}
-	}, [invulnerable]);
+  useEffect(() => {
+    if (invulnerable) {
+      setBlinkOn(true);
+    }
+  }, [invulnerable]);
 
-	return useSpring({
-		scale: poweredUp ? 2.4 : 1.4,
-		opacity: blinkOn ? 0 : 1,
-		onRest: () => {
-			if (invulnerable) {
-				setBlinkOn(!blinkOn);
-			}
-			if (!invulnerable && blinkOn) {
-				setBlinkOn(false);
-			}
-		},
-	});
+  return useSpring({
+    scale: poweredUp ? 2.4 : 1.4,
+    opacity: blinkOn ? 0 : 1,
+    onRest: () => {
+      if (invulnerable) {
+        setBlinkOn(!blinkOn);
+      }
+      if (!invulnerable && blinkOn) {
+        setBlinkOn(false);
+      }
+    },
+  });
 }
